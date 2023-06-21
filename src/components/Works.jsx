4063cbdbf9f78ef24401e3c2fe7b04733e44f014
rohan sharma <rohans540@gmail.com>
2023-06-21 12:27:47 +0530

    fixed works card width and edited experiences

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,7 +17,7 @@ import { projects } from '../constants'
           scale: 1,
           speed: 450
         }}
-        className="bg-tertiary p-5 rounded-2xl sm: w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className='relative w-full h-[230px] '>
           <img 
